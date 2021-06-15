d7aa7246a0a1604de33a88ef8ff60322fe5b4a4c
CyberJalagam <jaishnavprasad@disroot.org>
2021-06-15 04:18:05 +0000

    cipher: Improve banner a bit
     * Now displays in red colour
    
    Signed-off-by: CyberJalagam <jaishnavprasad@disroot.org>

--- build/tools/cipher.sh
+++ build/tools/cipher.sh
@@ -1,5 +1,15 @@
-echo "
+RESET='\033[0m'           # Text Reset
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
 
+echo -e "${RED}"
+echo "
  ▄████▄   ██▓ ██▓███   ██░ ██ ▓█████  ██▀███      ▒█████    ██████ 
 ▒██▀ ▀█  ▓██▒▓██░  ██▒▓██░ ██▒▓█   ▀ ▓██ ▒ ██▒   ▒██▒  ██▒▒██    ▒ 
 ▒▓█    ▄ ▒██▒▓██░ ██▓▒▒██▀▀██░▒███   ▓██ ░▄█ ▒   ▒██░  ██▒░ ▓██▄   
@@ -10,5 +20,7 @@ echo "
 ░         ▒ ░░░        ░  ░░ ░   ░     ░░   ░    ░ ░ ░ ▒  ░  ░  ░  
 ░ ░       ░            ░  ░  ░   ░  ░   ░            ░ ░        ░  
 ░                                                                  
-
 "
+echo ""
+echo -e "${PURPLE}"
+echo "Thanks for building CipherOS!"
