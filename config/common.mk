2fba1d086221c1cd9441ad81d1dd32d640762852
Aryan Sinha <sinha.aryan03@gmail.com>
2021-12-16 14:15:48 +0530

    cipher: Warn when building Go Edition
    * If accidently set, notify builder that its building CipherOS Go Edition.
    
    Change-Id: I80702296e491c080ad5a20e9a4d56a4d26de8721
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -234,6 +234,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # CipherOS Go Edition
 ifeq ($(CIPHER_GO), true)
     $(call inherit-product, build/make/target/product/go_defaults.mk)
+    $(warning CIPHER_GO is defined, building CipherOS Go Edition)
     CIPHER_BUILD_ZIP_TYPE := GO
     PRODUCT_PACKAGES += \
     Launcher3QuickStepGo
