b91f77e41c8db1c784d990fc9644462a01341553
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:43:52 +0300

    config: Remove TARGET_HAS_LEGACY_CAMERA_HAL1
    Change-Id: I6f4cbe41f6df3ceb6b4058d5b6e77c02e1899809

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,7 +32,6 @@ SOONG_CONFIG_lineageGlobalVars += \
     bootloader_message_offset \
     disable_postrender_cleanup \
     gralloc_handle_has_reserved_size \
-    has_legacy_camera_hal1 \
     has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
@@ -65,7 +64,6 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
-SOONG_CONFIG_lineageGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_lineageGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_lineageGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
