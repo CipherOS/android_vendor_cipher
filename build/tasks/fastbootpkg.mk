bea4202874da42bd670fa5d4ee542d9784ea8bed
techyminati <sinha.aryan03@gmail.com>
2023-01-29 13:05:56 +0530

    build: tasks: fixup!: `fastbootpkg` build task
    Change-Id: I0b9094914e1286c4e1e15483a3015ed32e1fed21
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- build/tasks/fastbootpkg.mk
+++ build/tasks/fastbootpkg.mk
@@ -1,6 +1,4 @@
-# Copyright (C) 2017 Unlegacy-Android
-# Copyright (C) 2017,2020 The LineageOS Project
-# Copyright (C) 2021 CipherOS Project
+# Copyright (C) 2020-2023 CipherOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,17 +13,16 @@
 # limitations under the License.
 
 # -----------------------------------------------------------------
-# CipherOS OTA update package
+# CipherOS fastboot package
 
-CIPHER_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CIPHER_VERSION).zip
+CIPHER_TARGET_FASTBOOT_PACKAGE := $(PRODUCT_OUT)/$(CIPHER_VERSION)-FASTBOOT.zip
 
 MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
-.PHONY: bacon
-bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CIPHER_TARGET_PACKAGE)
-	$(hide) $(MD5) $(CIPHER_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CIPHER_TARGET_PACKAGE).md5sum
-	@echo "Package Complete: $(CIPHER_TARGET_PACKAGE)" >&2
-	@echo "Build is getting done..." >&2
-	$(hide) bash vendor/cipher/build/tools/cipher.sh
-
+.PHONY: fastbootpkg
+fastbootpkg: $(INTERNAL_UPDATE_PACKAGE_TARGET)
+              $(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(CIPHER_TARGET_FASTBOOT_PACKAGE)
+              $(hide) $(MD5) $(CIPHER_TARGET_FASTBOOT_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CIPHER_TARGET_FASTBOOT_PACKAGE).md5sum
+              @echo "Fastboot Package Complete: $(CIPHER_TARGET_FASTBOOT_PACKAGE)" >&2
+              @echo "Build is getting done..." >&2
+              $(hide) bash vendor/cipher/build/tools/cipher.sh
