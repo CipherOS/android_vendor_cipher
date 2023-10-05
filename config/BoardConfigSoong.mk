b24815ab3273f754983b834c08b80844cb9bd70b
techyminati <sinha.aryan03@gmail.com>
2023-10-05 17:45:49 +0200

    test
    Change-Id: I64143439119e65e597c307cb5050861ba2ccdb95

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -2,7 +2,7 @@ PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
 
 # Add variables that we wish to make available to soong here.
 EXPORT_TO_SOONG := \
-    KERNEL_ARCH \
+ #   KERNEL_ARCH \
     KERNEL_BUILD_OUT_PREFIX \
     KERNEL_CROSS_COMPILE \
     KERNEL_MAKE_CMD \
@@ -21,7 +21,7 @@ SOONG_CONFIG_lineageVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_lineageVarsPlugin += $(1)
-  SOONG_CONFIG_lineageVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_lineageVarsPlugin_$$(1) := $$(subst \",\\\",$$($$1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
