0383e3b14ffa8d42277875641e5666261692bb51
alibei <sebastiankotzias83@gmail.com>
2023-10-12 00:43:44 +0200

    cipher: Cleanup fonts
    * Remove duplicate build rules
    * Remove duplicate/unneeded files
    
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -159,11 +159,6 @@ PRODUCT_PACKAGES += \
     wget \
     zip
 
-# System fonts
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/cipher/prebuilt/fonts/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
-    vendor/cipher/prebuilt/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
-
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.exfat \
--- config/fonts.mk
+++ config/fonts.mk
@@ -1,5 +1,5 @@
 # Fonts
-LOCAL_PATH := vendor/cipher/fonts
+LOCAL_PATH := vendor/cipher/prebuilt/fonts
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 
 PRODUCT_COPY_FILES += \
