f59ba8dc2fa6878d6c0db13ed8bee25de61bd438
techyminati <sinha.aryan03@gmail.com>
2025-06-13 12:51:12 +0530

    cipher: Enable Material Design 3 Expressive * Also guard it with flag
    Change-Id: Ib6d3ac2e0f3d76768c508e1b00efc51e141c1b09
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -339,10 +339,18 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/cipher/config/partner_gms.mk
 
+
 # SystemUIClocks
 PRODUCT_USES_SYSTEMUICLOCKS ?= false
 ifeq ($(PRODUCT_USES_SYSTEMUICLOCKS), true)
     $(call inherit-product, vendor/SystemUIClocks/product.mk)
+endif
+
+# Enable Material Design 3 Expressive
+CIPHER_EXPRESSIVE ?= true
+ifeq ($(CIPHER_EXPRESSIVE), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
 endif
 
 # Inherit vendor/cipher-extras if exists
